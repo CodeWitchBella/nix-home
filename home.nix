89e850b0f3448008bedec83215e18baabff1250a
Isabella Skořepová <isabella@skorepova.info>
2025-05-07 22:59:03 +0200

    add nixd

--- home.nix
+++ home.nix
@@ -24,6 +24,7 @@ in
     enable = true;
     extensions = ["nix"];
     package = config.lib.nixGL.wrap pkgs.zed-editor;
+    extraPackages = [ pkgs.nixd ];
     userSettings = {
       features = {
         copilot = false;
